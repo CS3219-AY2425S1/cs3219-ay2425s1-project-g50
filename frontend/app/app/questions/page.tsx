f35ca128f4ecec5ae8ecf713e8e6141bf18b0f7e
TanPingZhi <josiahtpz@gmail.com>
2024-09-25 20:51:08 +0800

    1

--- frontend/app/app/questions/page.tsx
+++ frontend/app/app/questions/page.tsx
@@ -1,5 +1,6 @@
 import QuestionListing from "@/components/questions/questions-listing";
-
+// import some random garbage
+import { useEffect } from "react";
 export default function QuestionListingPage() {
   return <QuestionListing />;
 }
